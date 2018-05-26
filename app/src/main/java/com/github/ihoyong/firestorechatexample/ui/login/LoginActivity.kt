e082a49bd2dbcbe562a2a902766accfd310c7695
이호용 <lee199402@gmail.com>
2018-05-26 22:22:38 +0900

    create login activity

---
+++ app/src/main/java/com/github/ihoyong/firestorechatexample/ui/login/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.github.ihoyong.firestorechatexample.ui.login
+
+import com.github.ihoyong.firestorechatexample.R
+import com.github.ihoyong.firestorechatexample.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : BaseActivity() {
+    override fun getResID(): Int = R.layout.activity_login
+
+
+    override fun init() {
+    }
+
+    override fun resume() {
+    }
+
+    override fun pause() {
+    }
+
+    override fun destroy() {
+    }
+
+}
