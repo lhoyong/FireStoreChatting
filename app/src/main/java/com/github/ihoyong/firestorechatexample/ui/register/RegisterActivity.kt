25d8ea8a6079bd703b3552a84a4eb4eddeffcfaa
이호용 <lee199402@gmail.com>
2018-05-27 00:23:14 +0900

    register add firebase auth

--- app/src/main/java/com/github/ihoyong/firestorechatexample/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/github/ihoyong/firestorechatexample/ui/register/RegisterActivity.kt
@@ -2,6 +2,7 @@ package com.github.ihoyong.firestorechatexample.ui.register
 
 import com.github.ihoyong.firestorechatexample.R
 import com.github.ihoyong.firestorechatexample.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : BaseActivity() {
 
@@ -11,6 +12,13 @@ class RegisterActivity : BaseActivity() {
 
     override fun init() {
         presenter = RegisterPresenter()
+
+        register_submit.setOnClickListener {
+            presenter.submit(register_id.text.toString().trim(), register_pw.text.toString().trim()) {
+                if (it) finish(
+                )
+            }
+        }
     }
 
     override fun resume() {
--- app/src/main/java/com/github/ihoyong/firestorechatexample/ui/register/RegisterPresenter.kt
+++ app/src/main/java/com/github/ihoyong/firestorechatexample/ui/register/RegisterPresenter.kt
@@ -1,7 +1,34 @@
 package com.github.ihoyong.firestorechatexample.ui.register
 
-import com.github.ihoyong.firestorechatexample.ui.login.Contract
+import com.google.firebase.auth.FirebaseAuth
 
-class RegisterPresenter: Contract.Presenter {
+class RegisterPresenter : Contract.Presenter {
+
+    private lateinit var view: Contract.View
+
+    override fun attachView(view: Contract.View) {
+        this.view = view
+    }
+
+    override fun deathView() {
+
+    }
+
+    override fun submit(id: String, pw: String, callback: (Boolean) -> Unit) {
+
+        if (id.isNotEmpty() && pw.isNotEmpty()) {
+            FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(id, pw)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) callback(true)
+                        else callback(false)
+                    }.addOnFailureListener {
+                        it.printStackTrace()
+                    }
+        } else {
+            callback(false)
+        }
+
+    }
 
 }
